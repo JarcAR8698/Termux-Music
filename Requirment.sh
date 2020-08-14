184335ef4fb9d157a92639fa54c51f973536b449
JarcAR8698 <69320287+JarcAR8698@users.noreply.github.com>
2020-08-14 16:06:53 +0530

    Add files via upload

---
+++ Requirment.sh
@@ -0,0 +1,9 @@
+
+#Installing Required Packages For Termux-Music
+apt-get update && apt-get upgrade -y
+pkg install toilet -y
+pkg install figlet -y
+pkg install ruby -y
+gem install lolcat -y
+pkg install python2 -y
+pip install youtube-dl
